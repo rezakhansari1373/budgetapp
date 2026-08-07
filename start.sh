29518ee2d23f0f35a6c0c8e25b9fe83161241ef8
reza <reza@budgetapp.local>
2026-08-08 01:36:36 +0330

    Remove cron job from blueprint; generate recurring on startup

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@ set -o errexit
 
 python manage.py migrate
 python manage.py seed_categories
+python manage.py generate_recurring
 python manage.py create_admin
 gunicorn budgetapp.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 2
